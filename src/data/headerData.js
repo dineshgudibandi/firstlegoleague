5c334b8d748f962ef3fbbf64a729e3fb177c1874
Dinesh <dineshreddyy@gmail.com>
2022-04-01 17:10:28 -0700

    updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Parvathaneni Princess',
     title: "Student",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared.",
-    image: './assets/images/nishi.jpeg',
+    image: 'https://raw.githubusercontent.com/dineshgudibandi/firstlegoleague/main/src/assets/images/nishi.jpeg',
     resumePdf: '',
 }
